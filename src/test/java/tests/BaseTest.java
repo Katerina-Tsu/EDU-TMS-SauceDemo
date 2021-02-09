164bd2ba4d57c3e250d1842ec48964f34f99045b
Katerina-Tsu <k.tsupikova@outlook.com>
2021-02-09 17:24:26 +0300

    Have a problem with addProductToCart

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,11 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
+import pages.ProductsPage;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
+    LoginPage loginPage;
+    ProductsTests productsPage;
 
     @BeforeMethod
     public void iniTTest() {
@@ -17,6 +21,10 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        //1.Open page()
+        loginPage = new LoginPage(driver);
+        //2.Add product to cart
+        productsPage = new ProductsPage(driver);
     }
 
     @AfterMethod(alwaysRun = true)
--- src/test/java/tests/ProductsTests.java
+++ src/test/java/tests/ProductsTests.java
@@ -1,11 +1,17 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductsPage;
+
 public class ProductsTests extends BaseTest{
 
+    @Test
     public void addProductToCartTest() {
-        //1.Open page()
-        //2.Login()
+        loginPage.openPage();
+        loginPage.login(loginPage.USERNAME, loginPage.PASSWORD); //2.login page
         //3.Add product to cart()
+        productsPage.addProductToCart();
         //4.Open CartPage()
         //5.Assert quantity()
         //6.Assert price()
